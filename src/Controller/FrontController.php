f15aa1037cd6639ffa4544a99d32bba84ed339d6
ayoub <titan.ayoub28@gmail.com>
2024-04-17 06:43:14 +0100

    front

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SubscriptionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    private $subscriptionRepository;
+
+    public function __construct(SubscriptionRepository $subscriptionRepository)
+    {
+        $this->subscriptionRepository = $subscriptionRepository;
+    }
+
+    #[Route('/front', name: 'app_front')]
+    public function index(): Response
+    {
+        $subscriptions = $this->subscriptionRepository->findAll();
+
+        return $this->render('front/index.html.twig', [
+            'subscriptions' => $subscriptions,
+        ]);
+    }
+}

fb4d648fa5eab85d79ca9df225a559ee2b541cd3
ayoub <titan.ayoub28@gmail.com>
2024-04-24 02:09:53 +0100

    stripe api works perfectly

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use App\Repository\SubscriptionRepository;
 
 class PaymentController extends AbstractController
 {
@@ -22,7 +23,7 @@ class PaymentController extends AbstractController
 
 
     #[Route('/checkout', name: 'checkout')]
-    public function checkout(string $stripeSK): Response
+    public function checkouttest(string $stripeSK): Response
     {
         Stripe::setApiKey($stripeSK);
 
@@ -61,4 +62,45 @@ class PaymentController extends AbstractController
     {
         return $this->render('payment/cancel.html.twig', []);
     }
+
+    private $subscriptionRepository;
+
+    public function __construct(SubscriptionRepository $subscriptionRepository)
+    {
+        $this->subscriptionRepository = $subscriptionRepository;
+    }
+
+    #[Route('/checkout/{id}', name: 'checkout')]
+    public function checkout(int $id, string $stripeSK): Response
+    {
+        $subscription = $this->subscriptionRepository->find($id);
+
+        if (!$subscription) {
+            throw $this->createNotFoundException('The subscription does not exist');
+        }
+
+        Stripe::setApiKey($stripeSK);
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => $subscription->getName(),
+                        ],
+                        'unit_amount' => $subscription->getPrice() * 100, // Stripe expects the amount in cents
+                    ],
+                    'quantity' => 1,
+                ]
+            ],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        return $this->redirect($session->url, 303);
+    }
+
 }

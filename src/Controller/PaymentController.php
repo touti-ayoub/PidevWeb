f3247dcecee7b61bd690612ee56d462ad70a2d41
zeineb <zeinebjmal445@gmail.com>
2024-05-02 02:56:53 +0100

    testing success api url

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -16,7 +16,7 @@ class PaymentController extends AbstractController
     #[Route('/payment', name: 'payment')]
     public function index(): Response
     {
-        return $this->render('payment/index.html.twig', [
+        return $this->render('payment/memberships.html.twig', [
             'controller_name' => 'PaymentController',
         ]);
     }
@@ -46,7 +46,7 @@ class PaymentController extends AbstractController
             'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
         ]);
 
-        dd($session);
+        return $this->redirect($session->url, 303);
     }
 
 
@@ -57,17 +57,46 @@ class PaymentController extends AbstractController
     }
 
 
-    #[Route('/cancel-url', name: 'cancel_url')]
-    public function cancelUrl(): Response
-    {
-        return $this->render('payment/cancel.html.twig', []);
-    }
+
 
     private $subscriptionRepository;
 
     public function __construct(SubscriptionRepository $subscriptionRepository)
     {
         $this->subscriptionRepository = $subscriptionRepository;
+    }
+
+    #[Route('/checkout/{id}', name: 'checkout')]
+    public function checkout(int $id, string $stripeSK): Response
+    {
+        $subscription = $this->subscriptionRepository->find($id);
+
+        if (!$subscription) {
+            throw $this->createNotFoundException('The subscription does not exist');
+        }
+
+        Stripe::setApiKey($stripeSK);
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => $subscription->getName(),
+                        ],
+                        'unit_amount' => $subscription->getPrice() * 100, // Stripe expects the amount in cents
+                    ],
+                    'quantity' => 1,
+                ]
+            ],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        return $this->redirect($session->url, 303);
     }
 
 }

8951c2e367cd95bf137250dc8a7084da68bfc41d
zeineb <zeinebjmal445@gmail.com>
2024-05-02 02:49:16 +0100

    testing api works

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -16,13 +16,38 @@ class PaymentController extends AbstractController
     #[Route('/payment', name: 'payment')]
     public function index(): Response
     {
-        return $this->render('payment/memberships.html.twig', [
+        return $this->render('payment/index.html.twig', [
             'controller_name' => 'PaymentController',
         ]);
     }
 
 
+    #[Route('/checkout', name: 'checkout')]
+    public function checkouttest(string $stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
 
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => 'T-shirt',
+                        ],
+                        'unit_amount' => 2000,
+                    ],
+                    'quantity' => 1,
+                ]
+            ],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        dd($session);
+    }
 
 
     #[Route('/success-url', name: 'success_url')]
@@ -43,39 +68,6 @@ class PaymentController extends AbstractController
     public function __construct(SubscriptionRepository $subscriptionRepository)
     {
         $this->subscriptionRepository = $subscriptionRepository;
-    }
-
-    #[Route('/checkout/{id}', name: 'checkout')]
-    public function checkout(int $id, string $stripeSK): Response
-    {
-        $subscription = $this->subscriptionRepository->find($id);
-
-        if (!$subscription) {
-            throw $this->createNotFoundException('The subscription does not exist');
-        }
-
-        Stripe::setApiKey($stripeSK);
-
-        $session = Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [
-                [
-                    'price_data' => [
-                        'currency' => 'usd',
-                        'product_data' => [
-                            'name' => $subscription->getName(),
-                        ],
-                        'unit_amount' => $subscription->getPrice() * 100, // Stripe expects the amount in cents
-                    ],
-                    'quantity' => 1,
-                ]
-            ],
-            'mode' => 'payment',
-            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
-            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
-        ]);
-
-        return $this->redirect($session->url, 303);
     }
 
 }
